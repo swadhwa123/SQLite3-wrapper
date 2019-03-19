a31e3491850f5d9f5b3740f1f4f0d69111d6caf1
cbettler <carlee.bettler@gmail.com>
2019-03-19 16:30:27 -0500

    Update 33_get_genename_R2.py

--- example_queries/33_get_genename_R2.py
+++ example_queries/33_get_genename_R2.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas
 pops = ["AFA", "AFHI", "ALL", "CAU", "HIS"]
+#Populations, African-American, African american hispanic, african american hispanic caucasian, caucasian, hispanic
 path = "/home/angela/MESA_dbs/"
 
 genename_R2 = []
@@ -8,6 +9,7 @@ for pop in pops:
     conn = sqlite3.connect(path + pop + "_imputed_10_peer_3_pcs_v2.db") #connect to .db file
     c = conn.cursor()
     c.execute('SELECT (`gene` || " " || `genename` || " " || `pred.perf.r2`) FROM EXTRA;') #run search throguh python
+    #Extra database stores gene by gene information 
     for row in c:
       row = row[0].split(" ") #tuple is just one element, so split into two
       genename_R2.append([row[0], row[1], row[2], pop]) #extract gene name and R2
