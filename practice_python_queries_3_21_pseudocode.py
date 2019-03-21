61bd14c728d3494f8d3b7feb6903dd0be58c78ff
Angela Andaleon <angie.andaleon@gmail.com>
2019-03-21 10:46:53 -0500

    Create practice_python_queries_3_21_pseudocode.py

---
+++ practice_python_queries_3_21_pseudocode.py
@@ -0,0 +1,28 @@
+import sqlite3
+import pandas as pd
+
+#which .db are we querying?
+#open connection to db file
+#start a list of lists to store data in (only have one dataframe to store this all in)
+#load in list of gene names you want to query
+  #APOE and PSRC1
+  #iterate through list
+#what are the names of these genes (starting w/ ENSG)?
+#what's its test R2 average?
+#what SNPs are in the file
+ #what are their rsids and weights?
+ 
+#make list of lists into dataframe
+#give column names so user knows what they're looking at
+#write to .csv
+
+
+
+
+
+
+
+
+
+
+
