9d6dedb59c934fa7fc9d2fc34ab3fa56780a7601
Angela Andaleon <angie.andaleon@gmail.com>
2019-03-28 10:48:09 -0500

    add argparse alternate

--- pseudocode_3_27.py
+++ pseudocode_3_27.py
@@ -1,114 +1,45 @@
-#this file can be run to take user input and pass it to the querying file
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 16:26:45 2019
+#Solution to example python queries
+#Run this program with: python practice_python_queries/Angela_solution_to_query_example.py --genenames practice_python_queries/genenames.txt --db db/gtex_v7_Whole_Blood_imputed_europeans_tw_0.5_signif.db
+@author: Angela
+"""
 
-#if the python script is run without any flags, just output genenames, cv_R2_avg, rsid, and weights (most common things we use)
+import argparse
+import numpy as np
+import sqlite3
+import pandas as pd
+
 parser = argparse.ArgumentParser()
+parser.add_argument("--db", type = str, action = "store", dest = "db", required = True, help = ".db you want to query.") #"db/gtex_v7_Whole_Blood_imputed_europeans_tw_0.5_signif.db"
+parser.add_argument("--genenames", type = str, action = "store", dest = "genenames", required = False, default = "genenames.txt", help = "File containing gene names.") #"practice_python_queries/genenames.txt"
+args = parser.parse_args()
 
-#string inputs
-#if none, query all available
-parser.add_argument("--db", type = str, action = "store", dest = "db", required = False, help = ".db you want to query.") #"db/gtex_v7_Whole_Blood_imputed_europeans_tw_0.5_signif.db"
-parser.add_argument("--genes", type = str, action = "store", dest = "genes", required = False, help = "File containing genes (Ensembl IDs).") #"practice_python_queries/genenames.txt"
-parser.add_argument("--genenames", type = str, action = "store", dest = "genenames", required = False, help = "File containing gene names.") #"practice_python_queries/genenames.txt"
+db = args.db  #which .db are we querying?
+genenames = np.loadtxt(args.genenames, dtype = str, ndmin = 1) #load in list of gene names you want to query
 
-#boolean inputs
-#EXTRA
-parser.add_argument("--n.snps.in.model", action = "store_true", dest = "n_snps_in_model", default = False, help = "Output the number of SNPs within the cis window that have non-zero weights, as found by elastic-net.")
-parser.add_argument("--test_R2_avg", action = "store_true", dest = "test_R2_avg", default = False, help = "Output the average coefficient of determination when predicting values of the hold out fold during nested cross validation.")
-parser.add_argument("--cv_R2_avg", action = "store_true", dest = "cv_R2_avg", default = False, help = "Output the average coefficient of determination for each of the hold out folds when cross-validation was performed on the entire data set.")
-parser.add_argument("--rho_avg", action = "store_true", dest = "rho_avg", default = False, help = "Output the average correlation between predicted and observed on the hold out folds when doing nested cross-validation.")
-parser.add_argument("--rho_zscore", action = "store_true", dest = "rho_zscore", default = False, help = "Output the transformation of rho_avg into a z-score using Stouffer's Method.")
-parser.add_argument("--pred.perf.R2", action = "store_true", dest = "pred_perf_R2", default = False, help = "Output the rho_avg squared.")
-parser.add_argument("--pred.perf.pval", action = "store_true", dest = "pred_perf_pval", default = False, help = "Output the p-value for rho_zscore.")
+'''
+db = "db/gtex_v7_Whole_Blood_imputed_europeans_tw_0.5_signif.db"  #which .db are we querying?
+genenames = np.loadtxt("practice_python_queries/genenames.txt", dtype = str, ndmin = 1) #load in list of gene names you want to query
+'''
 
-#WEIGHTS (this table depends on the EXTRA table)
-parser.add_argument("--rsid", action = "store_true", dest = "rsid", default = False, help = "Output the rsids in the models of queried genes.")
-parser.add_argument("--varID", action = "store_true", dest = "varID", default = False, help = "Output the variant IDs in the models of queried genes. These are string labels of the format chromosome_position_allele1_allele2_build. All varIDs are from build 37 of the HUman Reference Genome.")
-parser.add_argument("--ref_allele", action = "store_true", dest = "ref_allele", default = False, help = "Output the reference alleles of the SNPs in the models of the queried genes.")
-parser.add_argument("--eff_allele", action = "store_true", dest = "eff_allele", default = False, help = "Output the effect alleles of the SNPs in the models of the queried genes.")
-parser.add_argument("--weight", action = "store_true", dest = "weight", default = False, help = "Output the weights for the SNPs that are used to calculate predicted expression for the gene. In predicting the expression for the gene, the weight is multiplied by the count, or estimated count, of the effect allele in individual. This value is added to all other weighted SNPs in the model.")
+conn = sqlite3.connect(db) #open connection to db file
+c = conn.cursor()
+data = [] #start a list of lists to store data in (only have one dataframe to store this all in)
 
-#SAMPLE INFO
-parser.add_argument("--n_samples", action = "store_true", dest = "n_samples", default = False, help = "Output the number of samples used the make the .db file.")
-parser.add_argument("--population", action = "store_true", dest = "population", default = False, help = "Output the population studied.")
-parser.add_argument("--tissue", action = "store_true", dest = "tissue", default = False, help = "Output the tissue or MESA population from which RNA was sequenced.")
-
-#THRESHOLDS (may mess around with)
-parser.add_argument("--test_R2_avg_thres", type = float, dest = "test_R2_avg_thres", default = 0, help = "Restrict the test_R2_avg to values above this threshold.")
-parser.add_argument("--cv_R2_avg_thres", type = float, dest = "cv_R2_avg_thres", default = 0, help = "Restrict the cv_R2_avg to values above this threshold.")
-parser.add_argument("--rho_avg_thres", type = float, dest = "rho_avg_thres", default = 0, help = "Restrict the rho_avg to values above this threshold.")
-parser.add_argument("--pred.perf.R2", type = float, dest = "pred_perf_R2_thres", default = 0, help = "Restrict the test_R2_avg to values above this threshold.")
-parser.add_argument("--pred.perf.pval", type = float, dest = "pred_perf_pval_thres", default = 0, help = "Restrict the pred_perf_pval to values below this threshold.")
-
-args = parser.parse_args() #then pass these arguments to further things
-
-########
-#CARLEE#
-########
-
-###QUERYING 
-#NOTE: we'll just query EVERYTHING (aka use *) in SQL b/c I'm more comfortable parsing in pandas than in SQL
-#for db/, have the user direct to a FOLDER of db files
-  #OR have two db flags - one that iterates through a folder of .dbs and one that is for a specific .db file
-    #if ends w/ .db, it's a db (make a list of one)
-    #else find all the .dbs in that folder
-#have giant list to make into list of lists
-#iterate through each db file
-  #db file will be a column name in final output
-  #everything is encased in one big loop around a list of db files to parse
-
-  #if user did pass genes or genenames
-    #translates genenames to genes
-    #store list in genes variable
-  #else
-    #select gene from extra
-    #make all genes into a list
-
-  #iterate through each gene (see python practice example query stuff)
-    #if the user chose any general flags (anything from EXTRA, WEIGHTS, or SAMPLE INFO)
-
-      #if chose anything from extra
-        #select * from extra where gene is iterated gene
-          #it'll output a tuple
-          #store all these in variables to be used later on (at least the gene)
-      
-      #repeat these lines for the sample info flags
-
-      #if user chooses any weights flags
-        #select * from weights where gene is iterated gene
-          #it'll output a tuple
-          #store all these in variables to be used later on
-          #then append all information (all info in weights, all info in extra, all info in sample info flags) to list of lists
-
-  #else (user chooses no flags)
-    #get list of all genes in model
-    #iterate through each gene
-      #pull out cv_R2_avg
-      #search in weights for all of them
-        #pull rsid and weights
-
-########        
-#SHREYA#
-########
-
-###PARSING QUERY OUTPUT
-#alright so we're done querying shiz and we got a big boi list of lists
-#convert list of lists into dataframe
-
-#if user has flags 
-  #get everything "true" the user wants
-    #always include genename
-    #ex. if they want cv_r2_avg, rho_avg, and rsid, make a list of ["genename", "cv_r2_avg", "rho_avg", "rsid"]
-    
-#if user has no flags
-  #genename, cv_r2_avg, rs, and weights
-#give column names
-  #(this will all be in the same order so we just need to figure out what this order is)
-#only pull columns of what the user wants
-
-#restrict to thresholds the user wants (see threshold flags)
-  #ex. if only want cv_r2_avg > 0.1, only keep those
-  
-#delete duplicate rows
-#print to csv
-  #remove indexes cause they're annoying
-  
-
+for genename in genenames:
+    c.execute("select gene, cv_R2_avg from extra where genename = '" + genename + "';") #what are the names of these genes (starting w/ ENSG)?
+    for row in c:
+        gene = row[0]
+        cv_R2_avg = row[1] #what's its cv R2 average?
+    c.execute("select rsid, weight from weights where gene = '" + gene + "'") #what SNPs are in the file and what are their rsids and weights?
+    for row in c:
+        rs = row[0]
+        weight = row[1]
+        data.append([rs, weight, genename, cv_R2_avg])
+        
+data = pd.DataFrame(data) #make list of lists into dataframe
+data.columns = ["rs", "weight", "genename", "cv_R2_avg"] #give column names so user knows what they're looking at
+data.to_csv("querying_output.csv", index = False) #write to .csv; give user option to choose this
+print("Program is completed with querying. Have a nice day :)")
